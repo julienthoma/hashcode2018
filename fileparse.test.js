75c5636a747645667ef8206be80049c37969c494
Julien Thoma <julien.thoma89@gmail.com>
2018-03-01 19:08:29 +0100

    v2

--- fileparse.test.js
+++ fileparse.test.js
@@ -4,13 +4,6 @@ describe('fileparser', () => {
   it('parse file', () => {
     expect(true).toEqual(true);
 
-    const result = parseFile('./test.in');
-
-    expect(result.videoCount).toEqual(5);
-    expect(result.endpointCount).toEqual(2);
-    expect(result.requestDescCount).toEqual(4);
-    expect(result.cacheCount).toEqual(3);
-    expect(result.cacheCapacity).toEqual(100);
-
+    const result = parseFile('./a_example.in');
   });
 });
