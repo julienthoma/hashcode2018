b828149c4ecf33e0c8717a26ca4ec9cd381ec3da
Julien Thoma <julien.thoma89@gmail.com>
2018-03-03 00:12:30 +0100

    update

--- main.js
+++ main.js
@@ -1,22 +1,141 @@
-const fileparser = require('./fileparser');
+const fileparser = require("./fileparser");
 const parseFile = fileparser.parseFile;
 const writeOutput = fileparser.writeOutput;
 
-const run = (fileName) => {
-  const data = parseFile(`./${fileName}.in`);
+const calcDist = (pointA, pointB) =>
+  Math.abs(pointA.row - pointB.row) + Math.abs(pointA.column - pointB.column);
 
-  testVehicles = [
-      {
-        id: 1,
-        rides: [0]
-      },
-      {
-        id: 2,
-        rides: [2, 1]
+const calcVehicleRideDistance = (vehicle, ride) => {
+  const rideDistance = calcDist(ride.start, ride.finish);
+  const taxiDistance = calcDist(vehicle.currentPoint, ride.start);
+
+  return rideDistance + taxiDistance;
+};
+
+const createVehicles = (vehicleCount, steps) => {
+  const vehicles = [];
+  for (var i = 0; i < vehicleCount; i++) {
+    vehicles.push({
+      id: i,
+      rides: [],
+      currentPoint: { row: 0, column: 0 },
+      stepsLeft: steps
+    });
+  }
+  return vehicles;
+};
+
+const createVehicleRideCombinations = (vehicles, rides) => {
+  const rideCombinations = [];
+  rides.forEach(ride => {
+    vehicles.forEach(vehicle => {
+      rideCombinations.push({
+        rideId: ride.id,
+        vehicleId: vehicle.id,
+        score: 0
+      });
+    });
+  });
+  return rideCombinations;
+};
+
+const calcScore = (combination, rideMap, vehicleMap, steps, bonus) => {
+  const ride = rideMap[combination.rideId];
+  const vehicle = vehicleMap[combination.vehicleId];
+  const rideDist = calcDist(ride.start, ride.finish);
+  const totalDist = calcVehicleRideDistance(vehicle, ride);
+  const currentGameTime = steps - vehicle.stepsLeft;
+  let score = rideDist;
+
+  const distanceToRideStart = calcDist(vehicle.currentPoint, ride.start);
+  if (
+    totalDist > vehicle.stepsLeft ||
+    totalDist + currentGameTime > ride.latestFinish
+  ) {
+    return 0;
+  }
+
+  if (distanceToRideStart + currentGameTime === ride.earliestStart) {
+    score += bonus;
+  }
+
+  return score;
+};
+
+const run = fileName => {
+  console.time(fileName);
+  const { vehicles, rideCount, rides, steps, bonus } = parseFile(
+    `./${fileName}.in`
+  );
+
+  const _vehicles = createVehicles(vehicles, steps);
+
+  let rideCombinations = createVehicleRideCombinations(_vehicles, rides);
+
+  console.log(rides.length);
+
+  let count = 0;
+  const total = rides.length;
+
+  let lastVehicleId = -1;
+  let scoresInitialized = false;
+
+  while (true) {
+    console.time('duration');
+    // recalc score
+    rideCombinations.forEach(combination => {
+      if (combination.vehicleId !== lastVehicleId && scoresInitialized) {
+        return;
       }
-    ];
+      const currentVehicle = _vehicles[combination.vehicleId];
+      const currentRide = rides[combination.rideId];
 
-    writeOutput(`./${fileName}.out`, testVehicles);
-}
+      combination.score = calcScore(
+        combination,
+        rides,
+        _vehicles,
+        steps,
+        bonus
+      );
+      combination.stepsNeeded = calcVehicleRideDistance(
+        currentVehicle,
+        currentRide
+      );
+    });
 
-run('a_example');
+    scoresInitialized = true;
+
+    rideCombinations.sort((a, b) => b.score - a.score);
+    const currentRideCombination = rideCombinations.shift();
+
+    if (!currentRideCombination || currentRideCombination.score === 0) {
+      break;
+    }
+
+    const currentVehicle = _vehicles[currentRideCombination.vehicleId];
+    const currentRide = rides[currentRideCombination.rideId];
+    currentVehicle.rides.push(currentRideCombination.rideId);
+    currentVehicle.currentPoint = currentRide.finish;
+    currentVehicle.stepsLeft -= currentRideCombination.stepsNeeded;
+
+    count++;
+
+    rideCombinations = rideCombinations.filter(
+      combination => combination.rideId !== currentRideCombination.rideId
+    );
+
+    lastVehicleId = currentVehicle.id;
+
+    console.log(`${fileName} - rides: (${count}/${total})`);
+    console.timeEnd('duration');
+  }
+
+  writeOutput(`./${fileName}.out`, _vehicles);
+  console.timeEnd(fileName);
+};
+
+// run("a_example");
+// run("b_should_be_easy");
+run("c_no_hurry");
+run("d_metropolis");
+run("e_high_bonus");
--- main.test.js
+++ main.test.js
@@ -5,40 +5,21 @@ const writeOutput = fileparser.writeOutput;
 
 const createVehicles = (vehicleCount, steps) => {
   const vehicles = [];
-  for (var i = 1; i <= vehicleCount; i++) {
+  for (var i = 0; i < vehicleCount; i++) {
     vehicles.push({
       id: i,
       rides: [],
-      currentPoint: {
-        row: 0,
-        column: 0
-      },
+      currentPoint: { row: 0, column: 0 },
       stepsLeft: steps
     });
   }
-
   return vehicles;
 };
 
 const createVehicleRideCombinations = (vehicles, rides) => {
-  const vehicleRideCombinations = {};
-  const vehicleMap = {};
-  const rideMap = {};
   const rideCombinations = [];
-
-  vehicles.forEach(vehicle => {
-    vehicleMap[vehicle.id] = vehicle;
-  });
-
   rides.forEach(ride => {
-    rideMap[ride.id] = ride;
-    vehicleRideCombinations[ride.id] = {};
-
     vehicles.forEach(vehicle => {
-      vehicleRideCombinations[ride.id][vehicle.id] = {
-        score: 0
-      };
-
       rideCombinations.push({
         rideId: ride.id,
         vehicleId: vehicle.id,
@@ -46,28 +27,21 @@ const createVehicleRideCombinations = (vehicles, rides) => {
       });
     });
   });
-
-  return {
-    vehicleRideCombinations,
-    vehicleMap,
-    rideMap,
-    rideCombinations
-  };
+  return rideCombinations;
 };
 
 const calcScore = (combination, rideMap, vehicleMap, steps, bonus) => {
   const ride = rideMap[combination.rideId];
   const vehicle = vehicleMap[combination.vehicleId];
-
-  const rideDistance = calcDist(ride.start, ride.finish);
-  const totalDistance = calcVehicleRideDistance(vehicle, ride);
-  let score = rideDistance;
+  const rideDist = calcDist(ride.start, ride.finish);
+  const totalDist = calcVehicleRideDistance(vehicle, ride);
   const currentGameTime = steps - vehicle.stepsLeft;
-  
+  let score = rideDist;
+
   const distanceToRideStart = calcDist(vehicle.currentPoint, ride.start);
   if (
-    totalDistance > vehicle.stepsLeft ||
-    totalDistance + currentGameTime > ride.latestFinish
+    totalDist > vehicle.stepsLeft ||
+    totalDist + currentGameTime > ride.latestFinish
   ) {
     return 0;
   }
@@ -79,30 +53,6 @@ const calcScore = (combination, rideMap, vehicleMap, steps, bonus) => {
   return score;
 };
 
-const calcScoreForAllCombinations = (
-  rideCombinations,
-  vehicleMap,
-  rideMap,
-  steps,
-  bonus
-) => {
-  rideCombinations.forEach(combination => {
-    const currentVehicle = vehicleMap[combination.vehicleId];
-    const currentRide = rideMap[combination.rideId];
-
-    combination.score = calcScore(
-      combination,
-      rideMap,
-      vehicleMap,
-      steps,
-      bonus
-    );
-    combination.stepsNeeded = calcVehicleRideDistance(currentVehicle, currentRide);
-  });
-
-  return rideCombinations;
-};
-
 const run = fileName => {
   console.time(fileName);
   const { vehicles, rideCount, rides, steps, bonus } = parseFile(
@@ -110,91 +60,62 @@ const run = fileName => {
   );
 
   const _vehicles = createVehicles(vehicles, steps);
-  const resultVehicles = [];
 
-  let {
-    vehicleRideCombinations,
-    vehicleMap,
-    rideMap,
-    rideCombinations
-  } = createVehicleRideCombinations(_vehicles, rides);
+  let rideCombinations = createVehicleRideCombinations(_vehicles, rides);
 
-  calcScoreForAllCombinations(
-    rideCombinations,
-    vehicleMap,
-    rideMap,
-    steps,
-    bonus
-  );
-
-  console.log(rideCombinations);
+  console.log(rides.length);
 
   while (true) {
-    rideCombinations.sort((a,b) => b.score - a.score);
+    // recalc score
+    rideCombinations.forEach(combination => {
+      const currentVehicle = _vehicles[combination.vehicleId];
+      const currentRide = rides[combination.rideId];
+
+      combination.score = calcScore(
+        combination,
+        rides,
+        _vehicles,
+        steps,
+        bonus
+      );
+      combination.stepsNeeded = calcVehicleRideDistance(
+        currentVehicle,
+        currentRide
+      );
+    });
+
+    rideCombinations.sort((a, b) => b.score - a.score);
     const currentRideCombination = rideCombinations.shift();
 
-    console.log(rideCombinations.length);
-    console.log(currentRideCombination);
+    if (!currentRideCombination || currentRideCombination.score === 0) {
+      break;
+    }
 
     const currentVehicle = _vehicles[currentRideCombination.vehicleId];
-    const currentRide = rideMap[currentRideCombination.rideId];
+    const currentRide = rides[currentRideCombination.rideId];
     currentVehicle.rides.push(currentRideCombination.rideId);
     currentVehicle.currentPoint = { ...currentRide.finish };
     currentVehicle.stepsLeft -= currentRideCombination.stepsNeeded;
 
-    rideCombinations = rideCombinations.filter(combination => combination.rideId === currentRideCombination.rideId);
+    rideCombinations = rideCombinations.filter(
+      combination => combination.rideId !== currentRideCombination.rideId
+    );
 
-    break;
+    console.log(currentRide.id);
   }
 
-  console.log(rideCombinations);
+  console.log(rides);
+  console.log(_vehicles);
 
-  // for (let i = 0; i < rideCount; i++) {
-  //   const currentRide = rides[i];
-
-  //   for (let j = 0; j < vehicles; j++) {
-  //     const currentVehicle = resultVehicles[j];
-
-  //     const expectedSteps = calcVehicleRideDistance(
-  //       currentVehicle,
-  //       currentRide
-  //     );
-  //     const currentGameTime = steps - currentVehicle.stepsLeft;
-  //     const distanceToRideStart = calcDist(
-  //       currentVehicle.currentPoint,
-  //       currentRide.start
-  //     );
-
-  //     console.log(
-  //       distanceToRideStart,
-  //       currentGameTime,
-  //       expectedSteps,
-  //       currentRide.earliestStart
-  //     );
-
-  //     if (
-  //       expectedSteps <= currentVehicle.stepsLeft &&
-  //       expectedSteps + currentGameTime <= currentRide.latestFinish &&
-  //       distanceToRideStart + currentGameTime === currentRide.earliestStart
-  //     ) {
-  //       currentVehicle.stepsLeft -= expectedSteps;
-  //       currentVehicle.currentPoint = { ...currentRide.finish };
-  //       currentVehicle.rides.push(currentRide.id);
-  //       currentRide.isAssigned = true;
-  //       break;
-  //     }
-  //   }
-  // }
-
-  writeOutput(`./${fileName}.out`, resultVehicles);
+  writeOutput(`./${fileName}.out`, _vehicles);
   console.timeEnd(fileName);
 };
 
 describe("main", () => {
   it("example", () => {
-    run("a_example");
+    // run("a_example");
     // run("b_should_be_easy");
-    // run("c_no_hurry");
+    run("c_no_hurry");
     // run("d_metropolis");
     // run("e_high_bonus");
   });
